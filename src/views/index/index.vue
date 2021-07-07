db8df465c210cf002a6d32271b3b4a9d9a4b10d4
pfcoder <pfcoder97@gmail.com>
2021-07-07 10:28:21 +0800

    remove unused

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -3,7 +3,7 @@
     <el-row :gutter="20">
       <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
         <el-alert v-if="noticeList[0]" :closable="noticeList[0].closable">
-          <div
+          <!--<div
             style="display: flex; align-items: center; justify-content: center"
           >
             <a
@@ -16,7 +16,7 @@
               />
             </a>
             <p v-html="noticeList[0].title"></p>
-          </div>
+          </div>-->
         </el-alert>
       </el-col>
       <el-col :xs="24" :sm="24" :md="12" :lg="6" :xl="6">
@@ -93,111 +93,6 @@
         </router-link>
       </el-col>
 
-      <el-col :xs="24" :sm="24" :md="24" :lg="11" :xl="11">
-        <el-card class="card" shadow="never">
-          <div slot="header">
-            <span>依赖信息</span>
-            <div style="float: right">部署时间:{{ updateTime }}</div>
-          </div>
-          <div class="bottom-btn">
-            <el-popover placement="top" width="250" trigger="hover">
-              <p>
-                请我们喝杯咖啡，付款后联系qq
-                783963206，我们将邀请您加入我们的讨论群，谢谢您愿意支持开源，加群获取文档、及基础模板，群内大佬众多，希望能帮到大家（如情况不允许，请勿勉强）。
-              </p>
-              <el-image :src="require('@/assets/zfb_kf.jpg')"></el-image>
-              <a slot="reference" target="_blank">
-                <el-button type="primary">QQ讨论群、基础版、文档</el-button>
-              </a>
-            </el-popover>
-            <a
-              target="_blank"
-              href="https://github.com/chuzhixin/vue-admin-beautiful"
-            >
-              <el-button type="warning">github下载源码点star</el-button>
-            </a>
-            <a
-              target="_blank"
-              href="https://gitee.com/chu1204505056/vue-admin-beautiful"
-            >
-              <el-button type="warning">码云下载源码点star</el-button>
-            </a>
-            <a @click="handleChangeTheme">
-              <el-button type="danger">修改主题和布局</el-button>
-            </a>
-            <!--  <el-popover placement="top" width="250" trigger="hover">
-              <p>谢谢您愿意支持开源，加群获取文档，群内提供基础模板</p>
-              <el-image :src="require('@/assets/ewm.png')"></el-image>
-              <a slot="reference" target="_blank">
-                <el-button type="warning">文档</el-button>
-              </a>
-            </el-popover> -->
-          </div>
-          <table class="table">
-            <tr>
-              <td>@vue/cli版本</td>
-              <td>{{ devDependencies['@vue/cli-service'] }}</td>
-              <td>vue版本</td>
-              <td>{{ dependencies['vue'] }}</td>
-            </tr>
-            <tr>
-              <td>vuex版本</td>
-              <td>{{ dependencies['vuex'] }}</td>
-              <td>vue-router版本</td>
-              <td>{{ dependencies['vue-router'] }}</td>
-            </tr>
-            <tr>
-              <td>element-ui版本</td>
-              <td>{{ dependencies['element-ui'] }}</td>
-              <td>axios版本</td>
-              <td>{{ dependencies['axios'] }}</td>
-            </tr>
-            <tr>
-              <td>eslint版本</td>
-              <td>{{ devDependencies['eslint'] }}</td>
-              <td>prettier版本</td>
-              <td>{{ devDependencies['prettier'] }}</td>
-            </tr>
-            <tr>
-              <td>sass版本</td>
-              <td>{{ devDependencies['sass'] }}</td>
-              <td>mockjs版本</td>
-              <td>{{ dependencies['mockjs'] }}</td>
-            </tr>
-            <tr>
-              <td>zx-layouts版本</td>
-              <td>{{ dependencies['zx-layouts'] }}</td>
-              <td>lodash版本</td>
-              <td>{{ dependencies['lodash'] }}</td>
-            </tr>
-          </table>
-        </el-card>
-
-        <el-card shadow="never">
-          <div slot="header">
-            <span>其他信息</span>
-          </div>
-          <div style="text-align: center">
-            <vab-colorful-icon style="font-size: 140px" icon-class="vab" />
-            <h1 style="font-size: 30px">vue-admin-beautiful</h1>
-          </div>
-          <div v-for="(item, index) in noticeList" :key="index">
-            <el-alert
-              v-if="index !== 0"
-              :title="item.title"
-              :type="item.type"
-              :closable="item.closable"
-            ></el-alert>
-            <br />
-          </div>
-          <el-alert :closable="false" :title="userAgent" type="info"></el-alert>
-          <br />
-        </el-card>
-
-        <plan></plan>
-        <version-information></version-information>
-      </el-col>
-
       <el-col :xs="24" :sm="24" :md="13" :lg="13" :xl="13">
         <el-card class="card" shadow="never">
           <div slot="header">
@@ -222,18 +117,11 @@
 <script>
   import VabChart from '@/plugins/echarts'
   import { dependencies, devDependencies } from '../../../package.json'
-  import { getList } from '@/api/changeLog'
-  import { getNoticeList } from '@/api/notice'
-  import { getRepos, getStargazers } from '@/api/github'
-  import Plan from './components/Plan'
-  import VersionInformation from './components/VersionInformation'
 
   export default {
     name: 'Index',
     components: {
       VabChart,
-      Plan,
-      VersionInformation,
     },
     data() {
       return {
@@ -580,7 +468,7 @@
         this.$baseEventBus.$emit('theme')
       },
       async fetchData() {
-        const { data } = await getList()
+        /*const { data } = await getList()
         data.map((item, index) => {
           if (index === data.length - 1) {
             item.color = '#0bbd87'
@@ -588,7 +476,7 @@
         })
         this.activities = data
         const res = await getNoticeList()
-        this.noticeList = res.data
+        this.noticeList = res.data*/
         /* getRepos({
         token: "1061286824f978ea3cf98b7b8ea26fe27ba7cea1",
       }).then((res) => {
