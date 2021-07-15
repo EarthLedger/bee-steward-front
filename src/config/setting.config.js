3c9afd6becf8717124e112abcf408516e3c202d7
pfcoder <pfcoder97@gmail.com>
2021-07-15 12:34:47 +0800

    udpate config

--- src/config/setting.config.js
+++ src/config/setting.config.js
@@ -16,7 +16,7 @@ const setting = {
   baseURL:
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000/api/v1'
-      : 'http://localhost:3000/api/v1',
+      : 'http://36.250.0.19:7575/api/v1',
   //标题 （包括初次加载雪花屏的标题 页面的标题 浏览器的标题）
   title: 'SWARM 节点管理',
   //简写
